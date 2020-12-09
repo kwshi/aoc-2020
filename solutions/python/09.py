9e8737f3130b46b300de3e1a50077b38a113f657
Kye Shi <shi.kye@gmail.com>
2020-12-09 09:22:20 -0800

    day9 python

---
+++ solutions/python/09.py
@@ -0,0 +1,43 @@
+import sys
+import collections as co
+import itertools as it
+import pprint as pp
+
+def bad(nums, pre):
+    last = co.deque(nums[:pre])
+    sums = co.defaultdict(int)
+    for m, n in it.combinations(last, 2):
+        sums[m + n] += 1
+
+    for n in nums[pre:]:
+        if sums[n] == 0:
+            return n
+        k = last.popleft()
+        for m in last:
+            sums[k + m] -= 1
+            sums[m + n] += 1
+        last.append(n)
+
+def find(nums, n):
+    acc = i = j = 0
+    while (acc != n or i + 1 == j) and j < len(nums):
+        if acc < n:
+            acc += nums[j]
+            j += 1
+        elif acc > n:
+            acc -= nums[i]
+            i += 1
+        else: 
+            # single-entry sum
+            i += 1
+            j += 1
+    
+    return i, j
+
+
+nums = [*map(int, sys.stdin)]
+b = bad(nums, 25)
+print(b)
+
+i, j = find(nums, b)
+print(min(nums[i:j]) + max(nums[i:j]))
