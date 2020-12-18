031b5aefdd3c968528fa3d3d6c8048e889e9ea6d
Kye Shi <shi.kye@gmail.com>
2020-12-18 08:22:33 -0800

    day 18: dirty trick, via python's own parser + eval + str.replace

--- solutions/python/18.py
+++ solutions/python/18.py
@@ -7,6 +7,28 @@ import re
 import sys
 import math
 import enum
+import ast
+
+class Transform(ast.NodeTransformer):
+    def visit_Call(self, _):
+        raise ValueError('disallowing function calls, for safety')
+
+class Transform1(Transform):
+    def visit_Sub(self, _):
+        return ast.Mult()
+
+class Transform2(Transform):
+    def visit_Mult(self, _):
+        return ast.Add()
+
+    def visit_Add(self, _):
+        return ast.Mult()
+
+def expr1_ast(s):
+    return eval(ast.unparse(Transform1().visit(ast.parse(s.replace('*', '-'), mode='eval'))))
+
+def expr2_ast(s):
+    return eval(ast.unparse(Transform2().visit(ast.parse(s.replace('*', '_').replace('+', '*').replace('_', '+'), mode='eval'))))
 
 ops = {'*': op.mul, '+': op.add}
 
@@ -39,6 +61,6 @@ def sum_parse(p, ls):
     return sum(p(l)[0] for l in ls)
 
 lines = [line.strip().replace(' ', '') for line in sys.stdin]
-print(sum_parse(expr1, lines))
-print(sum_parse(expr2, lines))
+print(sum_parse(expr1, lines), sum(map(expr1_ast, lines)))
+print(sum_parse(expr2, lines), sum(map(expr2_ast, lines)))
 
